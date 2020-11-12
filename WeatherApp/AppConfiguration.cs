815f463e91f1c828134ce7e039a89bec3b94c3b0
Alexis Michaud <michaud2alexis@hotmail.com>
2020-11-12 01:30:20 -0700

    corrected secret and small modifs

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+
+        public static string GetValue(string v)
+        {
+            if (configuration == null)
+                initConfig();
+            return configuration.GetSection("Secrets")[v];
+        }
+
+        private static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", true, true);
+
+
+            builder.AddUserSecrets<MainWindow>();
+            configuration = builder.Build();
+            
+
+        }
+    }
+}
---
+++ WeatherApp/Services/OpenWeatherService.cs
@@ -0,0 +1,45 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Converter;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Services
+{
+    public class OpenWeatherService : ITemperatureService
+    {
+        private static OpenWeatherProcessor owp;
+
+        public TemperatureModel LastTemp;
+
+
+        public OpenWeatherService(string apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+            owp.ApiKey = apiKey;
+        }
+
+
+        public async Task<TemperatureModel> GetTempAsync()
+        {
+            var currentWeather = await owp.GetCurrentWeatherAsync();
+            TemperatureModel temp = new TemperatureModel();
+
+            //long l = Convert.ToInt64(DateTime.UnixEpoch);
+            // theres a shit about the time hour (pc stuck 2hrs too early)
+            DateTime dttime = new DateTime(1970,1,1,0,0,0, DateTimeKind.Utc);
+
+            temp.DateTime = dttime.AddSeconds(currentWeather.DateTime).ToLocalTime();
+
+            temp.Temperature = currentWeather.Main.Temperature;
+            TemperatureConverter.FahrenheitInCelsius(temp.Temperature);
+
+            LastTemp = temp;
+            return temp;
+
+        }
+    }
+}
